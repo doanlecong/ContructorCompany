8295fdd66051aab13aadad84a1ae3b6fdffe6b25
doanlecong <doanlecong1811@gmail.com>
2017-08-06 19:54:02 +0700

    Add main page to project

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('index','MainPageController@index');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
